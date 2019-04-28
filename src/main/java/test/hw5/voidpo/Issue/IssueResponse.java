62366e9c5b6b88bc1774d3f8674fb58868be037b
Elena <helenfilova@gmail.com>
2019-04-28 23:29:25 +0300

    hw5

---
+++ src/main/java/test/hw5/voidpo/Issue/IssueResponse.java
@@ -0,0 +1,24 @@
+package test.hw5.voidpo.Issue;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class IssueResponse {
+
+    public static final String ISSUE_KEY = "issue";
+
+    @JsonProperty(ISSUE_KEY)
+    private final Issue issue;
+
+    @JsonCreator
+    public IssueResponse(
+            @JsonProperty(ISSUE_KEY) Issue issue)
+    {
+        this.issue = issue;
+    }
+
+    @JsonProperty(ISSUE_KEY)
+    public Issue getIssue() {
+        return issue;
+    }
+}
---
+++ src/main/java/test/hw5/voidpo/User/FullUser.java
@@ -0,0 +1,24 @@
+package test.hw5.voidpo.User;
+
+public final class FullUser {
+
+    private final User user;
+    private final String password;
+
+    public FullUser(
+            final User user,
+            final String passsword
+    ){
+        this.user = user;
+        this.password = passsword;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
